524fe974f63daccb393e58454ac321c4b91cf8c2
elmol <eliasmolini@gmail.com>
2023-04-03 23:37:22 -0300

    monorepo: fixing web-app yarn script

--- apps/web-app/src/pages/_app.tsx
+++ apps/web-app/src/pages/_app.tsx
@@ -14,11 +14,11 @@ export default function App({ Component, pageProps }: AppProps) {
             <Head>
                 <title>ZK Proof of Humanity</title>
                 <link rel="icon" href="/favicon.ico" />
-                {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="manifest" href="/manifest.json" />
-                <meta name="theme-color" content="#ebedff" /> */}
+                <meta name="theme-color" content="#ebedff" />
             </Head>
 
             <ChakraProvider theme={theme}>
