abd720cd05253d4ee4f3a03e1526bd327d2d4208
elmol <eliasmolini@gmail.com>
2023-04-03 23:21:45 -0300

    fixing web-app lint issue

--- apps/web-app/src/styles/components/index.ts
+++ apps/web-app/src/styles/components/index.ts
@@ -2,6 +2,7 @@ import Button from "./Button"
 import Link from "./Link"
 import Tooltip from "./Tooltip"
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
     Link,
     Button,
