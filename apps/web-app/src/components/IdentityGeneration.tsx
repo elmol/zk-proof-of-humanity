4189a38e70c6783c4ca13f620abd5b480c362a14
elmol <eliasmolini@gmail.com>
2023-04-19 20:20:44 -0300

    widget: NewIdentity  button abstraction to widget

--- apps/web-app/src/components/IdentityGeneration.tsx
+++ apps/web-app/src/components/IdentityGeneration.tsx
@@ -1,44 +1,64 @@
 import LogsContext from "@/context/LogsContext";
-import { Button } from "@chakra-ui/react";
 import { Identity } from "@semaphore-protocol/identity";
 import { verifyMessage } from "ethers/lib/utils";
 import { useContext, useEffect } from "react";
 import { useSignMessage } from "wagmi";
 
+
+import theme from "../styles/index";
+
+import { BaseButton } from "zkpoh-button";
+import { ButtonActionProps, ButtonActionState } from "./ButtonAction";
+
 const message = "zk-proof-of-humanity";
 
-type Props = {
-  handleNewIdentity: (credential:{ identity: Identity, address: `0x${string}`}) => void
-};
+export interface NewIdentityProps extends ButtonActionProps {
+    handleNewIdentity: (credential: { identity: Identity; address: `0x${string}` }) => void;
+}
 
+export function IdentityGenerator(props: NewIdentityProps) {
+    const { data, error, isLoading, signMessage } = useSignMessage({
+        message,
+        onSuccess(data, variables) {
+            const identity = new Identity(data);
+            const address = verifyMessage(variables.message, data);
+            props.handleNewIdentity({ identity: identity, address: address as `0x${string}` });
+            props.onStateChange && props.onStateChange({ logs: "Your new Semaphore identity was just created 🎉" });
+        },
+    });
 
-export function IdentityGeneration(props:Props) {    
+    useEffect(() => {
+        props.onStateChange && props.onStateChange({ logs: "Generate your Semaphore identity 👆🏽" });
+    }, [props]);
 
-  const { setLogs } = useContext(LogsContext)
+    useEffect(() => {
+        error && props.onStateChange && props.onStateChange({ error: error, logs: "💻💥 Error: " + error.message });
+    }, [error, props]);
 
-  const { data, error, isLoading, signMessage } = useSignMessage({
-    message,
-    onSuccess(data, variables) {
-      const identity = new Identity(data);
-      const address = verifyMessage(variables.message, data);
-      props.handleNewIdentity({identity:identity, address:address as`0x${string}`});
-      setLogs("Your new Semaphore identity was just created 🎉")
-    },
-  });
+    console.log("*** Rendering Identity Generation");
 
-  useEffect(() => {
-    setLogs("Generate your Semaphore identity 👆🏽")
-  }, [setLogs])
-  
-  useEffect(() => {
-     error && setLogs('💻💥 Error: ' +  error.message )
-  }, [setLogs,error])
- 
-  return (
-    <>
-      <Button colorScheme="primary" isLoading={isLoading} loadingText='Check wallet' onClick={() => signMessage()}>
-          {isLoading ? "Check Wallet" : "Generate Identity"}
-      </Button>
-    </>
-  );
+    return (
+        <>
+            <BaseButton theme={props.theme} isLoading={isLoading} onClick={() => signMessage()}>
+                {props.children ? props.children : isLoading ? "Check Wallet" : "Generate Identity"}
+            </BaseButton>
+        </>
+    );
+}
+
+export function IdentityGeneration({
+    handleNewIdentity,
+}: {
+    handleNewIdentity: (credential: { identity: Identity; address: `0x${string}` }) => void;
+}) {
+    const { setLogs } = useContext(LogsContext);
+    function handleStateChange(state: ButtonActionState) {
+        setLogs(state.logs);
+    }
+
+    return (
+        <>
+            <IdentityGenerator theme={theme} onStateChange={handleStateChange} handleNewIdentity={handleNewIdentity} />
+        </>
+    );
 }
