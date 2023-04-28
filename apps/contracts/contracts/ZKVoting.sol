e0c944270388f2083cb683f8b5e54782df7861cd
emarino81 <emarino81@gmail.com>
2023-04-28 18:03:37 -0300

    ZKVote: add multi vote

--- apps/contracts/contracts/ZKVoting.sol
+++ apps/contracts/contracts/ZKVoting.sol
@@ -7,8 +7,22 @@ contract ZKVoting {
  
     string public message = "Voting system for president";
     uint256 public messageId;
+    mapping(uint256 => string) public polls;
+    uint256[] public keys;
 
-    constructor( uint256 _messageId) {
+
+    constructor( uint256 _messageId ) {
         messageId = _messageId;
+        polls[_messageId] = message;
+        keys.push(_messageId);
+    }
+
+    function addToMapping(uint256 _messageId, string memory _message) public {
+        polls[_messageId] = _message;
+        keys.push(_messageId);
+    }
+
+    function getAllKeys() public view returns (uint256[] memory) {
+        return keys;
     }
 }
--- apps/contracts/test/ZKVoting.ts
+++ apps/contracts/test/ZKVoting.ts
@@ -11,6 +11,7 @@ describe("ZKVoting", () => {
     const users: any = []
     const groupId = "42"
     const externalNullifer = randomNullifier()
+    const externalNullifer2 = randomNullifier();
     const group = new Group(groupId)
 
     before(async () => {
@@ -21,16 +22,33 @@ describe("ZKVoting", () => {
 
         const ContractFactory = await ethers.getContractFactory("ZKVoting")
 
-        contract = await ContractFactory.deploy(zkPoHContract.address, externalNullifer)
+        contract = await ContractFactory.deploy(externalNullifer)
     })
 
     describe("# construction", () => {
         it("Should be constructed with hardcoded message", async () => {
-            expect("The house is in order").equal(await contract.message())
+            expect("Voting system for president").equal(await contract.message())
         })
 
         it("Should be constructed with an message ID (externalNullifier)", async () => {
             expect(externalNullifer).equal(await contract.messageId())
+        })
+
+        it("Should be constructed with hardcoded message with mapping", async () => {
+            expect("Voting system for president").equal(await contract.polls(externalNullifer))
+        })
+    })
+ 
+    describe("# Add Polls", () => {
+        it("Should allow adding a poll to the mapping", async () => {
+            await contract.addToMapping(externalNullifer2,"addpoll");
+            const cantidad = await contract.getAllKeys();
+            expect(2).equal(cantidad.length)
+        })
+
+        it("You should search by key in the mapping", async () => {
+             const _poll = await contract.polls(externalNullifer2);
+             expect("addpoll").equal(_poll)
         })
     })
 })
